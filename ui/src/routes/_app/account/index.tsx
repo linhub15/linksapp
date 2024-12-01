d59e3af8e45bb570a2ed6c8a617900c4cc43e55e
Hubert Lin <linhub15@users.noreply.github.com>
2024-11-30 22:59:54 -0700

    feat: account settings page

--- ui/src/routes/_app/account/index.tsx
+++ ui/src/routes/_app/account/index.tsx
@@ -1,9 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Heading, Subheading } from "../../../components/ui/heading.tsx";
+import { useProfile } from "../../../lib/auth/use_profile.ts";
+import { Input } from "../../../components/ui/input.tsx";
 
 export const Route = createFileRoute("/_app/account/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Account Page</div>;
+  const { data } = useProfile();
+  return (
+    <div className="max-w-3xl mx-auto">
+      <Heading>Account Settings</Heading>
+      <hr className="my-10 mt-6 w-full border-t border-zinc-950/10 dark:border-white/10" />
+
+      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div>
+          <Subheading>Your email</Subheading>
+          <p className="text-base/6 text-zinc-500 sm:text-sm/6 dark:text-zinc-400">
+            Email associated with Google
+          </p>
+        </div>
+        <div>
+          <Input type="email" value={data?.profile.email} disabled />
+        </div>
+      </section>
+    </div>
+  );
 }
